6d015a89c842f2448e99f7ecae6b7daed326f7a5
Juan David Velasquez <jdvr1994@hotmail.com>
2018-11-17 10:46:30 -0500

    Update WSExample.js

--- WSExample.js
+++ WSExample.js
@@ -49,8 +49,8 @@ raspberryPiCamera.on('frame', (frameData) => {
 
 // start capture
 options = {
-  width: 320,
-  height: 240,
+  width: 480,
+  height: 360,
   fps: 5,
   encoding: 'JPEG',
   quality: 10
